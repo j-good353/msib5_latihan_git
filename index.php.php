96936dc7637e907dfbd8daade3139c4d7206d763
j-good353 <140032518+j-good353@users.noreply.github.com>
2023-08-23 22:11:30 +0700

    init: add index.php

---
+++ index.php.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil Saya</title>
+    <!-- Sertakan Bootstrap CSS di sini jika diperlukan -->
+    <style>
+        body {
+            background-color: pink;
+            background-image: url('kucing.png'); /* Ganti 'kucing.png' dengan path gambar emoji kucing Anda */
+            background-repeat: repeat;
+        }
+        .container {
+            background-color: rgba(255, 255, 255, 0.8);
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0px 0px 10px 2px rgba(0,0,0,0.5);
+            text-align: center;
+        }
+        h1 {
+            font-size: 36px;
+            margin-bottom: 20px;
+            color: #282525;
+        }
+        .info {
+            margin-bottom: 20px;
+            font-size: 20px;
+        }
+        .info label {
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Profil Saya</h1>
+        <hr>
+
+        <?php
+        // Data profil Anda
+        $nama = "Jagad Deedat";
+        $kampus = "Universitas Brawijaya";
+        $jurusan = "Ilmu Ekonomi";
+        $prodi = "Ekonomi Islam";
+        $hobi = "Lari dari kenyataan";
+        ?>
+
+        <div class="info">
+            <label for="nama">Nama:</label>
+            <p><?php echo $nama; ?></p>
+        </div>
+        <div class="info">
+            <label for="kampus">Kampus:</label>
+            <p><?php echo $kampus; ?></p>
+        </div>
+        <div class="info">
+            <label for="jurusan">Jurusan:</label>
+            <p><?php echo $jurusan; ?></p>
+        </div>
+        <div class="info">
+            <label for="prodi">Prodi:</label>
+            <p><?php echo $prodi; ?></p>
+        </div>
+        <div class="info">
+            <label for="hobi">Hobi:</label>
+            <p><?php echo $hobi; ?></p>
+        </div>
+        <!-- Tambahkan informasi lainnya sesuai kebutuhan di sini -->
+    </div>
+</body>
+</html>
